bba968ca7899bab80ab247d707e75c78f55e0cca
DamianoCasolari <damianocasolari@gmail.com>
2023-04-11 17:34:20 +0200

    delete console log

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,20 +11,21 @@ createApp({
         }
     },
     methods: {
-    },
-    mounted() {
-        for (let index = 0; index < this.numberOfEmails; index++) {
+        createEmails() {
+            for (let index = 0; index < this.numberOfEmails; index++) {
 
-            axios.get(this.url).then(response => {
-                let email = response.data.response
-                this.arrayEmails.push(email)
-            }).catch(error => this.error = error.message)
-            console.log(index);
-            if (index == this.numberOfEmails - 1) {
-                this.finishedCycle = true 
+                axios.get(this.url).then(response => {
+                    let email = response.data.response
+                    this.arrayEmails.push(email)
+                }).catch(error => this.error = error.message)
+                if (index == this.numberOfEmails - 1) {
+                    this.finishedCycle = true 
+                }
             }
         }
-        
+    },
+    mounted() {
+        this.createEmails()
     },
 
 }).mount('#app')
