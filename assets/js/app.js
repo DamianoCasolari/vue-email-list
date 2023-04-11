d1e21ea2052fdbe71fb589179ce68c590f297b86
DamianoCasolari <damianocasolari@gmail.com>
2023-04-12 00:38:37 +0200

    rainbow effect in h1

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,7 +8,7 @@ createApp({
             numberOfEmails: 10,
             error: "",
             timeLeft: "",
-            title:"Create 10 emails randomly"
+            title: "Create 10 emails randomly"
 
         }
     },
@@ -22,8 +22,7 @@ createApp({
                         let email = response.data.response;
                         console.log(email);
                         this.arrayEmails.push(email);
-                        if (index == this.numberOfEmails - 1)
-                        {this.timeLeft = performance.now() - start;}
+                        if (index == this.numberOfEmails - 1) { this.timeLeft = performance.now() - start; }
                     })
                     .catch(error => this.error = error.message);
 
@@ -31,20 +30,43 @@ createApp({
 
             }
         },
-        scaleLetters(){
-            let container_h1 = document.querySelector("h1") 
-          const arrayLetters =  this.title.split("")
-        //   arrayLetters.forEach((string) => {
-        //     container_h1 += `<span class="scaleLetter"> ${string}</span>`
-        //   })
-        arrayLetters.forEach((string) => {
-            const span = document.createElement("span");
-            span.textContent = string;
-            if (string != " ") {
-                span.classList.add("scaleLetter");
-            }
+        scaleLetters() {
+            let container_h1 = document.querySelector("h1")
+            const arrayLetters = this.title.split("")
+            //   arrayLetters.forEach((string) => {
+            //     container_h1 += `<span class="scaleLetter"> ${string}</span>`
+            //   })
+            arrayLetters.forEach((string) => {
+                const span = document.createElement("span");
+                span.textContent = string;
+                // if (string != " ") {
+                //     span.classList.add("scaleLetter");
+                // }
                 container_h1.appendChild(span);
-          });
+
+            });
+            const arrayTotalSpan = document.querySelectorAll("h1 span")
+            arrayTotalSpan.forEach((span) => {
+                span.addEventListener("mouseover", function (e) {
+                    if (e.target != " ") {
+                        span.classList.add("scaleLetter")
+                        span.previousElementSibling.classList.add("scaleLetter2")
+                        span.previousElementSibling.previousElementSibling.classList.add("scaleLetter5")
+                        span.nextElementSibling.classList.add("scaleLetter3")
+                        span.nextElementSibling.nextElementSibling.classList.add("scaleLetter4")
+                    }
+                })
+                span.addEventListener("mouseout", function (e) {
+                    if (e.target != " ") {
+                        span.classList.remove("scaleLetter")
+                        span.previousElementSibling.classList.remove("scaleLetter2")
+                        span.previousElementSibling.previousElementSibling.classList.remove("scaleLetter5")
+                        span.nextElementSibling.classList.remove("scaleLetter3")
+                        span.nextElementSibling.nextElementSibling.classList.remove("scaleLetter4")
+
+                    }
+                })
+            })
         }
         // hourglasstime(){
         //     const hourglassElement = document.querySelectorAll('.hourglass_container div')
